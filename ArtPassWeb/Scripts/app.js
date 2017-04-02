aaeec701f8905fe24c61b5c4aa1629ed22e1bbf8
NickLacunha <njpadinha@gmail.com>
2017-04-02 16:24:08 -0700

    Got inserts working.

--- ArtPassWeb/Scripts/app.js
+++ ArtPassWeb/Scripts/app.js
@@ -9,10 +9,11 @@
         Hospital: ko.observable(),
         Age: ko.observable(),
         EmailAddress: ko.observable(),
-
+        DaysStaying: ko.observable()
     }
 
     var registrantsUri = '/api/RegistrantModels/';
+    var hospitalsUri = 'api/HospitalModels/';
 
     function ajaxHelper(uri, method, data) {
         self.error(''); // Clear error message
@@ -33,14 +34,35 @@
         });
     }
 
+    function getHospitals() {
+        ajaxHelper(hospitalsUri, 'GET').done(function (data) {
+            self.hospitals(data);
+        });
+    }
+
     self.getBookDetail = function (item) {
         ajaxHelper(registrantsUri + item.RegistrantId, 'GET').done(function (data) {
             self.detail(data);
         });
     }
 
+    self.addRegistrant = function (formElement) {
+        var registrant = {
+            Name: self.newRegistrant.Name(),
+            HospitalId: self.newRegistrant.Hospital().HospitalId,
+            Age: self.newRegistrant.Age(),
+            EmailAddress: self.newRegistrant.EmailAddress(),
+            DaysStaying: self.newRegistrant.DaysStaying()
+        };
+
+        ajaxHelper(registrantsUri, 'POST', registrant).done(function (item) {
+            self.registrants.push(item);
+        });
+    }
+
     // Fetch the initial data.
     getAllRegistrants();
+    getHospitals();
 };
 
 ko.applyBindings(new ViewModel());
