df7cf10ae96021504d4e89846e379de4b95aadfa
NickLacunha <njpadinha@gmail.com>
2017-04-02 17:30:10 -0700

    Ready to start refactoring app.js a little

--- ArtPassWeb/Scripts/app.js
+++ ArtPassWeb/Scripts/app.js
@@ -1,10 +1,10 @@
-﻿var ViewModel = function () {
+﻿var RegistrantBaseViewModel = function () {  // currently refactoring to extract the insert logic for the public frontend
     var self = this;
-    self.registrants = ko.observableArray();
-    self.error = ko.observable();
-    self.detail = ko.observable();
+    self.registrants = ko.observableArray();  // only needed for retrieves
+    self.error = ko.observable();   // base
+    self.detail = ko.observable();  // base I think
     self.hospitals = ko.observableArray();
-    self.newRegistrant = {
+    self.newRegistrant = {      // only needed for inserts
         Name: ko.observable(),
         Hospital: ko.observable(),
         Age: ko.observable(),
@@ -12,10 +12,10 @@
         DaysStaying: ko.observable()
     }
 
-    var registrantsUri = '/api/RegistrantModels/';
-    var hospitalsUri = 'api/HospitalModels/';
+    var registrantsUri = '/api/RegistrantModels/';  // base
+    var hospitalsUri = 'api/HospitalModels/';       // base
 
-    function ajaxHelper(uri, method, data) {
+    function ajaxHelper(uri, method, data) {        // base
         self.error(''); // Clear error message
         return $.ajax({
             type: method,
@@ -28,25 +28,25 @@
         });
     }
 
-    function getAllRegistrants() {
+    function getAllRegistrants() {      // retrieve only
         ajaxHelper(registrantsUri, 'GET').done(function (data) {
             self.registrants(data);
         });
     }
 
-    function getHospitals() {
+    function getHospitals() {   // base
         ajaxHelper(hospitalsUri, 'GET').done(function (data) {
             self.hospitals(data);
         });
     }
 
-    self.getBookDetail = function (item) {
+    self.getBookDetail = function (item) {      // retrieves
         ajaxHelper(registrantsUri + item.RegistrantId, 'GET').done(function (data) {
             self.detail(data);
         });
     }
 
-    self.addRegistrant = function (formElement) {
+    self.addRegistrant = function (formElement) {  // inserts
         var registrant = {
             Name: self.newRegistrant.Name(),
             HospitalId: self.newRegistrant.Hospital().HospitalId,
@@ -56,13 +56,22 @@
         };
 
         ajaxHelper(registrantsUri, 'POST', registrant).done(function (item) {
-            self.registrants.push(item);
+            if (typeof self.registrants != 'undefined') {  // registrants will only be defined in admin-level modules
+                self.registrants.push(item);
+            }
         });
     }
 
     // Fetch the initial data.
-    getAllRegistrants();
-    getHospitals();
+    getAllRegistrants();  // only needed for retrieves
+    getHospitals();       // base
 };
 
-ko.applyBindings(new ViewModel());
+var RegistrantInsertViewModel = function () {
+    var self = this;
+    RegistrantBaseViewModel.call(self);
+
+
+};
+
+ko.applyBindings(new RegistrantBaseViewModel());
