20b7375c6b30ff6260787d3e2e7631db909bb943
NickLacunha <njpadinha@gmail.com>
2017-04-03 22:25:17 -0700

    Added bare bones controllers for new pages

---
+++ ArtPassWeb/Controllers/ManageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtPassWeb.Controllers
+{
+    public class ManageController : Controller
+    {
+        // GET: Manage
+        public ActionResult Index()
+        {
+            return Overview();
+        }
+
+        public ActionResult Overview()
+        {
+            ViewBag.Title = "Program Participant Overview";
+
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            ViewBag.Title = "Add Program Participant";
+
+            return View();
+        }
+    }
+}
